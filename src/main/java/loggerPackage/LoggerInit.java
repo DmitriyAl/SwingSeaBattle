a8a91931b11cb23d894bff20e04dac691017fed8
DmitriyAl <dvalbot@mail.ru>
2016-01-10 19:51:36 +0300

    slf4j - log4j bridge test

--- src/main/java/loggerPackage/LoggerInit.java
+++ src/main/java/loggerPackage/LoggerInit.java
@@ -1,15 +1,16 @@
 package loggerPackage;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by Dmitriy on 10.01.2016.
  */
 public class LoggerInit {
-    public static final Logger log = Logger.getLogger(LoggerInit.class);
+    public static final Logger log = LoggerFactory.getLogger(LoggerInit.class);
 
     public static void main(String[] args) {
-        log.info("logger test");
+        log.info("slf4j-log4j bridge logger test");
 
     }
 }
